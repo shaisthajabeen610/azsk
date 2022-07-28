d9d9ef30636c77f9ba86e90bb69491d1c2c3e4cf
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-07-28 15:26:25 +0530

    mychanges

--- scanner.ps1
+++ scanner.ps1
@@ -9,7 +9,7 @@ Connect-AzAccount -UseDeviceAuthentication
 # Import-Module AzSK 
 Install-Module -Name AzSK -Scope CurrentUser -AllowClobber -Force 
 
-import-module az.accounts
+import-module az.accounts -AllowClobber -Force 
 
 
 
