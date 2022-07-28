52608caf5e18d2f6befec8113ab5ca39e5e8b4ee
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-07-28 15:03:16 +0530

    edit

--- scanner.ps1
+++ scanner.ps1
@@ -1,7 +1,15 @@
-﻿   Connect-AzAccount 
+﻿  Install-Module Az -Force
+Import-Module Az   
+
+az upgrade
+Connect-AzAccount -UseDeviceAuthentication
  $Id = Get-Azsubscription
- Install-Module AzSK -Scope CurrentUser -SkipPublisherCheck 
+ 
+# Install-Module AzSK -Scope CurrentUser -SkipPublisherCheck  -Force
+# Import-Module AzSK 
+Install-Module -Name AzSK -Scope CurrentUser -AllowClobber -Force 
 Import-Module AzSK 
+Set-AzSKPolicySettings -AutoUpdate On
  foreach ($Ids in $Id)
  {
  $draft = Get-AzSKSubscriptionSecurityStatus -SubscriptionId $Ids.id
@@ -9,4 +17,5 @@ $children = Get-ChildItem -Filter *.csv $draft
 azcopy copy "$draft/$children" "https://azsk1.blob.core.windows.net/azsk/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2022-07-27T16:15:43Z&st=2022-07-27T08:15:43Z&spr=https,http&sig=8MXZcgy3yJBbaQGWTCME4ZpAOPFxBPB7OVUmjgFtPCI%3D" --recursive=true
  Remove-Item $draft
  }
+ 
  
